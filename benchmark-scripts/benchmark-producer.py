ae5e3a47b9bd7fbefafa2f59f2f6cb76ab543261
cranberrymuffin <aparna_natarajan@brown.edu>
2022-05-16 11:01:33 -0400

    create topic from python admin client

--- benchmark-scripts/benchmark-producer.py
+++ benchmark-scripts/benchmark-producer.py
@@ -5,6 +5,7 @@ import base64
 import uuid
 import pathlib
 import re
+from confluent_kafka.admin import AdminClient, NewTopic
 
 
 def getRandomId():
@@ -26,44 +27,63 @@ def setDefaultConfigs():
         producer_config.write(f"client.id={getRandomId()}")
 
 
-def processMetricResults(output):
-    recordsPerSecond = ""
-    mbPerSecond = ""
-
-    for line in output.split('\n'):
-        if line.count("records/sec") == 0:
-            continue
-        else:
-            datapoints = line.split(",")
-            throughout = datapoints[1]
-            decimalRe = '\d*\.?\d+'
-            results = re.findall(decimalRe, throughout)
-            recordsPerSecond = results[0]
-            mbPerSecond = results[1]
-
-    return recordsPerSecond, mbPerSecond
+class ProducerBenchmarker:
+    def __init__(self, data_file, delimiter, bootstrap_servers):
+        self.bootstrap_servers = bootstrap_servers
+        self.data_path = data_file
+        self.delimiter = delimiter
+        self.admin_client = AdminClient(
+            {'bootstrap.servers': bootstrap_servers,
+             'client.id': 'admin_client'}
+        )
 
 
-# TODO change from topic from hardcoded value to one generated from admin client
-def runBenchmarks(data_path, delimiter):
-    data_path = pathlib.Path(data_path).resolve()
-    producerConfig = pathlib.Path('producer.config').resolve()
-    num_lines = sum(1 for _ in open(data_path, 'r'))
-    benchmarkCmd = f"kafka-producer-perf-test --num-records {num_lines} --throughput -1 " \
-                   f"--producer.config {producerConfig} " \
-                   f"--print-metrics --payload-file {data_path} --topic GaoVh9wTS-Gykm5z2GEbPA"
-    if delimiter != '\n':
-        benchmarkCmd += f" --payload-delimiter {delimiter}"
-    result = subprocess.run(benchmarkCmd.split(' '), capture_output=True, text=True)
-    recPerSec, mbPerSec = processMetricResults(result.stdout)
-    print(f"{recPerSec} records/sec; {mbPerSec} mb/sec")
+    @staticmethod
+    def processMetricResults(output):
+        recordsPerSecond = ""
+        mbPerSecond = ""
+
+        for line in output.split('\n'):
+            if line.count("records/sec") == 0:
+                continue
+            else:
+                datapoints = line.split(",")
+                throughout = datapoints[1]
+                decimalRe = '\d*\.?\d+'
+                results = re.findall(decimalRe, throughout)
+                recordsPerSecond = results[0]
+                mbPerSecond = results[1]
+
+        return recordsPerSecond, mbPerSecond
+
+    def getAndCreateTopic(self):
+        topic = getRandomId()
+        self.admin_client.create_topics([NewTopic(topic, num_partitions=3, replication_factor=1)])
+        return topic
+
+    # TODO change from topic from hardcoded value to one generated from admin client
+    def runBenchmarks(self):
+        data_path = pathlib.Path(self.data_path).resolve()
+        num_lines = sum(1 for _ in open(data_path, 'r'))
+        topic = self.getAndCreateTopic()
+        benchmarkCmd = f"kafka-producer-perf-test --num-records {num_lines} --throughput -1 " \
+                       f"--producer-props bootstrap.servers={self.bootstrap_servers} "\
+                       f"--print-metrics --payload-file {data_path} --topic {topic}"
+        if self.delimiter != '\n':
+            benchmarkCmd += f" --payload-delimiter {self.delimiter}"
+        result = subprocess.run(benchmarkCmd.split(' '), capture_output=True, text=True)
+        recPerSec, mbPerSec = self.processMetricResults(result.stdout)
+        if result.stderr != '':
+            raise Exception(result.stderr)
+
+        print(f"{recPerSec} records/sec; {mbPerSec} mb/sec")
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Process producer benchmarking configurations')
     parser.add_argument('-data_file', type=dir_path, help='data file')
     parser.add_argument('-delimiter', type=str, default='\n', help='data file delimiter')
-
+    parser.add_argument('-bootstrap_servers', type=str)
     args = parser.parse_args()
-
-    runBenchmarks(args.data_file, args.delimiter)
+    print(args.delimiter)
+    ProducerBenchmarker(args.data_file, args.delimiter, args.bootstrap_servers).runBenchmarks()
